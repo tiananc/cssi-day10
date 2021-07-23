81404d9dca5e930014b3a37007c5923189323a35
Tiana <tiananclemons@gmail.com>
2021-07-23 17:18:06 +0000

    checkpoint 2

--- starter/public/js/main.js
+++ starter/public/js/main.js
@@ -1,2 +1,27 @@
 console.log("Script Running!");
 console.log(firebase.database().ref());
+
+const databaseRef = firebase.database().ref();
+const songsInput = document.querySelector("#songs");
+const addPlaylist = document.querySelector("#addPlaylist");
+const playlistTitle = document.querySelector("#playlistTitle");
+
+ addPlaylist.addEventListener("click", (e) => {
+     console.log("clicked!")
+     console.log(songsInput.value);
+
+     databaseRef.push({
+         title: playlistTitle.value,
+         songs: songsInput.value.split("\n")
+
+
+
+     }
+
+     )
+    });
+
+
+
+
+
